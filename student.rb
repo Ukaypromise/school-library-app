a0113913f3b8f36e058b12fd139f165855718f52
Ukaypromise <promiseukaimo@gmail.com>
2022-11-04 11:10:14 -0400

    Fix errors

--- student.rb
+++ student.rb
@@ -1,7 +1,7 @@
 require_relative './person'
 
 class Student < Person
-  attr_reader :type, :classroom
+  attr_reader :type #:classroom
 
   def initialize(age, classroom, name: 'Unknown', parent_permission: true, type: 'Student')
     super(age, name, parent_permission: parent_permission)
