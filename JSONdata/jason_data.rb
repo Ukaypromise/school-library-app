b9e160c3576c3adb214c670d9ba632cab3feec6a
Engr. Promise <85847249+Ukaypromise@users.noreply.github.com>
2022-11-08 21:16:08 +0100

    Add method to read people, store people and read rentals

--- JSONdata/jason_data.rb
+++ JSONdata/jason_data.rb
@@ -29,4 +29,72 @@ module HandleFile
         author: book.author})
     end
     write_json(array, 'JSONdata/books.json')
+  end
+
+  # Read books from stored books file
+  def read_books
+    parse_file = read_json('JSONdata/books.json')
+    parse_file.map do |book|
+      @books.push(Book.new(book['title'], book['author']))
+    end
+  end
+
+  # Store People in Json file
+  def store_people
+    array = []
+    @people.map do |person|
+      if person.instance_of? Student
+        array.push({
+          id:person.id,
+          type:person.class,
+          name: person.name,
+          age: person.age,
+          parent_permission: person.parent_permission,
+          class_room: person.class_room })
+      else
+        array.push({
+          id:person.id,
+          type:person.class,
+          name: person.name,
+          age: person.age,
+          specialization: person.specialization })
+      end
+    end
+    write_json(array, 'JSONdata/people.json')
+  end
+
+  # Read people from stored people file
+  def read_people
+    parse_file = read_json('JSONdata/people.json')
+    parse_file.map do |person|
+      if person['type'] == 'Teacher'
+        teacher = Teacher.new(person['specialization'], person['age'], name: person['name'])
+        teacher.id = person['id']
+        @people.push(teacher)
+      else
+        student = Student.new(person['class_room'], person['age'], person['name'], parent_permission: person['parent_permission'])
+        student.id = person['id']
+        @people.push(student)
+      end
+    end
+  end
+
+  # Store Rentals in Json file
+  def store_rentals
+    array = []
+    @rentals.each do |rental|
+      array.push({
+        date: rental.date,
+        person_id: rental.person.id,
+        book_title: rental.book.title,
+        person_name: rental.person.name })
+    end
+    write_json(array, 'JSONdata/rentals.json')
+  end
+
+  # Read rentals from stored rentals file
+  def read_rentals
+  end
+
+
 end
