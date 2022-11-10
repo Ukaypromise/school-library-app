48af7b0cd1e0626127bb8d4a0ba1296da41eceb6
Ukaypromise <promiseukaimo@gmail.com>
2022-11-10 05:47:35 -0500

    Write test for person class

---
+++ spec/person_spec.rb
@@ -0,0 +1,21 @@
+require_relative '../person'
+
+person = Person.new(30, 'Addis')
+
+describe '#Review Person Class' do
+  it 'Returns Name' do
+    expect(person.name).to eql 'Addis'
+  end
+
+  it 'Returns Age' do
+    expect(person.age).to eql 30
+  end
+
+  it 'Return result for can_use_services?' do
+    expect(person.can_use_services?).to eql true
+  end
+
+  it 'Return result for correct_name' do
+    expect(person.correct_name).to eql 'Addis'
+  end
+end
