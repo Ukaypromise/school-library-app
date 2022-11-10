9d0bfae1cc714bd88fa901887f519dee5c71553e
Ukaypromise <promiseukaimo@gmail.com>
2022-11-10 07:49:35 -0500

    Remove unused block of code

--- spec/classroom_spec.rb
+++ spec/classroom_spec.rb
@@ -1,26 +1,5 @@
-# require './classroom'
-# require './student'
-
-# classroom = Classroom.new('A')
-
-# describe 'Classroom' do
-#     it 'Should have a label' do
-#         expect(classroom.label).to eql('A')
-#     end
-#     it 'Should have a students' do
-#         expect(classroom.students).to eql([])
-#     end
-#     it 'Should have a add_student method' do
-#         new_student = classroom.add_student('John')
-#         expect(new_student).to eql(['john'])
-#     end
-# end
-
 require './student'
 require './classroom'
-require './book'
-require './rentals'
-require 'json'
 
 classroom = Classroom.new('A')
 student = Student.new(25, 'A')
